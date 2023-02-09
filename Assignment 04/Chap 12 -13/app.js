c05dbd71ee4fac59f78e7f90e6bc52b0d0f69f26
Syed Umer Shah <101082333+SyedUmerShah@users.noreply.github.com>
2023-02-09 19:49:13 +0500

    Add files via upload

---
+++ Assignment 04/Chap 12 -13/app.js
@@ -0,0 +1,96 @@
+// // Q1
+// var char = prompt("")
+// var charCode = char.charCodeAt ()
+// if (charCode >= 65 && charCode <= 90 ) {
+//     alert ("Capital")
+  
+    
+// }
+// else if (charCode >= 97 && charCode <= 122) {
+//     alert("Small")
+// }
+// else{
+//     alert("Number")
+// }
+
+
+// Q2
+// var num1 = +prompt("Enter Num 01")
+// var num2 = +prompt("Enter Num 02")
+
+// if (num1 > num2) {
+//     alert(num1 + " Is Greater")
+    
+// }
+// else if (num2 > num1){
+//     alert (num2 + " Is Greater")
+// }
+// else{alert(num1 + " And " + num2 + " Are Equal")}
+
+
+// Q3
+// var num = +prompt("Enter A Number")
+
+
+// if (num > 0) {alert(num + " Is Positive")
+    
+// }
+// else if (num < 0){
+//     alert(num + " Is Negative")
+// }
+// else{
+//     alert("The Number Is " + num)
+// }
+
+
+// Q4
+
+// var a = "a"
+// var e = "e"
+// var i = "i"
+// var o = "o"
+// var u = "u"
+// var vowels = prompt("Type An Alphabet")
+// if (a == vowels || e == vowels || i == vowels || o ==vowels || u==vowels) {alert(vowels + " Is Vowels")
+    
+// }
+// else {
+//     alert(vowels + " Is Not Vowels")
+// }
+
+// Q5
+// var password = "umershah"
+// var entPass = prompt("Enter Your Password")
+// if (entPass == "") {
+//     document.write("Please! Enter Your Password")
+// }
+// else if (password == entPass){
+//     document.write("“Correct! The password you entered matches the original password”")
+// }
+// else{
+//     document.write("Incorrect Password")
+// }
+
+
+// Q7
+// var greeting;
+// var hour = 13;
+// if (hour < 18) 
+// {alert ("Good day")}
+
+// else{
+// alert ("Good evening")
+// }
+
+// Q8
+// var time = +prompt("Time")
+// if (time >=0000 && time < 1200) {document.write ("Good Morning")    
+// }
+// else if (time >=1200 && time < 1700 )
+// {document.write ("Good Afternoon")}
+
+// else if (time >= 1700 && time < 2100){document.write ("Good Evening")}
+// else if (time >= 2100 && time <=2359)
+// {document.write ("Good Morning")}
+
+// else {document.write ("Time Is InCorrect")}
