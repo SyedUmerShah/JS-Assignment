f0d38b5ed2519474f0314702b4b2cc29a7e29091
Syed Umer Shah <101082333+SyedUmerShah@users.noreply.github.com>
2023-02-14 14:58:59 +0500

    Add files via upload

---
+++ Assignment 05/Chap 14 -16/app.js
@@ -0,0 +1,56 @@
+// Q1
+// var names = [""]
+
+
+// Q2
+// var narr = new Array ("ahad", "meer")
+
+
+// Q3
+// var names = ["umer", "anus", "naveed", "ahmed"]
+
+
+// Q4
+// var num = [1, 2, 3, 4, 5,]
+
+
+// Q5
+// var boolean = [true, false]
+
+
+// Q6
+// var mixed = [true,  false, 1, 2, 3, "umer", "naveed"]
+
+
+// Q7
+// var qual = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PHD"]
+// document.write(1 + " " + qual[0] +"<br>" +
+// 2 + " " + qual[1] +"<br>" + 
+// 3 + " " + qual[2] +"<br>" +
+// 4 + " " + qual[2] +"<br>" +
+// 5 + " " + qual[3] +"<br>" +
+// 6 + " " + qual[4] +"<br>" +
+// 7 + " " + qual[5] +"<br>" +
+// 8 + " " + qual[6] +"<br>" )
+
+
+// Q8
+// var stud = [prompt("write student name 1")]
+// var index = [prompt("write index 1")]
+
+
+
+
+
+// Q9
+var color = ["red", "green", "blue"]
+var newcol = prompt("What color do want to add at the begining")
+
+
+
+
+
+
+
+
+
