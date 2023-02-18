8a273aa39d4ff0a4dc3a579465cb5bef0e2c5f24
Syed Umer Shah <101082333+SyedUmerShah@users.noreply.github.com>
2023-02-18 15:37:40 +0500

    Add files via upload

---
+++ Assignment 05/Chap 17 -20/app.js
@@ -0,0 +1,24 @@
+// Q1
+// var mult = [[],[],[]]
+
+
+
+
+// Q2
+// var mult = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+// document.write(mult[0].join("") + "<br>"+
+// mult[1].join("") + "<br>" + mult[2].join(""))
+
+// Q3
+// for(var i = 1;  i <= 10 ; i++){
+//     document.write(i)
+// }
+
+
+// Q4
+var tabnum = prompt("Enter table Num")
+var tabmul = prompt("Enter Table Multiplication")
+
+for (var i = tabnum ; i < tabmul; i++){
+document.write(i*tabmul)
+}
