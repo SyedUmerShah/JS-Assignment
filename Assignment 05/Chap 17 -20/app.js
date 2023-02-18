be9cdaf2193db29c30ddecf47e9b65b86c276c62
Syed Umer Shah <101082333+SyedUmerShah@users.noreply.github.com>
2023-02-18 19:28:37 +0500

    Update app.js

--- Assignment 05/Chap 17 -20/app.js
+++ Assignment 05/Chap 17 -20/app.js
@@ -1,3 +1,4 @@
+
 // Q1
 // var mult = [[],[],[]]
 
@@ -6,8 +7,8 @@
 
 // Q2
 // var mult = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
-// document.write(mult[0].join("") + "<br>"+
-// mult[1].join("") + "<br>" + mult[2].join(""))
+// document.write(mult[0].join(" ") + "<br>"+
+// mult[1].join(" ") + "<br>" + mult[2].join(" "))
 
 // Q3
 // for(var i = 1;  i <= 10 ; i++){
@@ -16,9 +17,12 @@
 
 
 // Q4
-var tabnum = prompt("Enter table Num")
-var tabmul = prompt("Enter Table Multiplication")
+// var tabnum = prompt("Enter table Num")
+// var tabmul = prompt("Enter Table Multiplication")
+// for(var i = 1; i <= tabmul;  i++ ){
+//     document.write(tabnum + " x " + i + " = " + tabnum*i +"<br>"   )
+// }
 
-for (var i = tabnum ; i < tabmul; i++){
-document.write(i*tabmul)
+for (var i = 0; i > -1; i++){
+    console.log(i)
 }
